1abf373a8dfede426d10fe2e4d10a37ffb3dbcf7
Ej Myles <ejelymyles@gmail.com>
2023-09-13 00:12:19 -0700

    add options to form select inputs

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -7,34 +7,52 @@ function Filter(){
           <label>
             Cuisine:
             <select>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
+              <option value="American">American</option>
+              <option value="BBQ">BBQ</option>
+              <option value="Chinese">Chinese</option>
+              <option value="Ethiopian">Ethiopian</option>
+              <option value="Filipino">Filipino</option>
+              <option value="French">French</option>
+              <option value="Hawaiian">Hawaiian</option>
+              <option value="Indian">Indian</option>
+              <option value="Italian">Italian</option>
+              <option value="Japanese">Japanese</option>
+              <option value="Korean">Korean</option>
+              <option value="Latin">Latin</option>
+              <option value="Mediterranean">Mediterranean</option>
+              <option value="Mexican">Mexican</option>
+              <option value="Middle Eastern">Middle Eastern</option>
+              <option value="Seafood">Seafood</option>
+              <option value="Soul Food">Soul Food</option>
+              <option value="Spanish">Spanish</option>
+              <option value="Steakhouse">Steakhouse</option>
+              <option value="Thai">Thai</option>
             </select>
           </label>
           <br />
           <label>
             Location:
             <select>
-              <option value="Downtown">Option 1</option>
-              <option value="">Option 2</option>
-              <option value="option3">Option 3</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
+              <option value="Downtown">Downtown</option>
+              <option value="Central LA">Central LA</option>
+              <option value="West LA">West LA</option>
+              <option value="East LA">East LA</option>
+              <option value="South LA">South LA</option>
+              <option value="North LA">North LA</option>
             </select>
           </label>
           <br />
           <label>
             Price:
             <select>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
-              {/* Add more price options as needed */}
+              <option value="$">$</option>
+              <option value="$$">$$</option>
+              <option value="$$$">$$$</option>
+              <option value="$$$$">$$$$</option>
             </select>
           </label>
           <br />
-          <button type="submit">Apply Filters</button>
+          <button type="submit">Search</button>
         </form>
       </div>
     );
