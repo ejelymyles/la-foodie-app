17858f7e2e55dfb19cc81801d241a51ef4b01525
Ej Myles <ejelymyles@gmail.com>
2023-09-13 23:35:51 -0700

    add state for data in App component. add useEffect to fetch and set state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Route, Routes } from 'react-router-dom';
-import NavBar from './Components/NavBar';
 import Home from './Components/HomePage';
 import RestaurantPage from './Components/RestaurantPage';
 import RestaurantForm from './Components/RestaurantForm';
 import Header from './Components/Header';
 
 function App() {
+
+const[resyData, setResyData] = useState([])
+
+useEffect(() => {
+  fetch("http://localhost:3000/restaurants")
+  .then((r) =>r.json())
+  .then((restaurants) => {
+    setResyData(restaurants);
+  })
+}, [])
+
   return (
     <div>
       <Header />
